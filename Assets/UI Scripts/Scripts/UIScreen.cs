d96023d88695a6ed1742d397c378c16d2edda503
Komal Sharma <komalsharma@Komals-MBP.home>
2019-09-24 18:34:54 -0400

    Adding UIScreen script to UISystem scene

--- Assets/UI Scripts/Scripts/UIScreen.cs
+++ Assets/UI Scripts/Scripts/UIScreen.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CanvasGroup))]
 public class UIScreen : MonoBehaviour
 {
 	#region Variables
+	[Header("Main Properties")]
+	public Selectable m_StartSelectables;
 
+	[Header("Screen Events")]
+	public UnityEvent onScreenStart = new UnityEvent();
+	public UnityEvent onScreenClose = new UnityEvent();
+
+
+	private Animator animator;
 	
 
 	#endregion
@@ -14,20 +27,41 @@ public class UIScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-    }
+       animator = GetComponent<Animator>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-
+       if(m_StartSelectables)
+       {
+       		EventSystem.current.SetSelectedGameObject(m_StartSelectables.gameObject);
+       }
     }
 
     #endregion
 
     #region Helper Methods
+    public virtual void StartScreen()
+    {
+    	if(onScreenStart != null){
+    		onScreenStart.Invoke();
+    	}
 
+    	HandleAnimator("show");
+    }
+
+    public virtual void CloseScreen()
+    {
+    	if(onScreenClose != null){
+    		onScreenClose.Invoke();
+    	}
+
+    	HandleAnimator("hide");
+    }
+
+    void HandleAnimator(string aTrigger)
+    {
+    	if(animator){
+    		animator.SetTrigger(aTrigger);
+    	}
+    }
 
 
     #endregion
