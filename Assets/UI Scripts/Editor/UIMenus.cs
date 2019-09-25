75e8ac9e218fb531c8dfe1c2f92dd1a5b0589165
Komal Sharma <komalsharma@Komals-MBP.home>
2019-09-24 20:23:53 -0400

    Added MenuItem for UI Tools

---
+++ Assets/UI Scripts/Editor/UIMenus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UIMenus : MonoBehaviour
+{
+    [MenuItem("UI System/UI Tools/Create UI Group")]
+    public static void CreateUIGroup(){
+    	GameObject uiGroup = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/UI_GRP.prefab");
+
+    	if(uiGroup)
+    	{
+    			GameObject createdGroup = (GameObject)Instantiate(uiGroup);
+    			createdGroup.name = "UI_GRP";
+    	}else
+    	{
+    		EditorUtility.DisplayDialog("UI Tools Warning", "Cannot find UI group prefab", "OK");
+    	}
+    }
+}
